6e628878a15a583f3c6e317f4c748553f6b4ca53
Trisha Aguinaldo <trisha.aguinaldo@gmail.com>
2019-03-16 08:37:41 -0400

    cheeses rendering

---
+++ db-mongoose.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const { DATABASE_URL } = require('./config');
+
+const dbConnect = ( url = DATABASE_URL) => {
+  return mongoose.connect(url, { useNewUrlParser: true })
+    .catch(err => {
+      console.error('Mongoose failed to connect');
+      console.error(err);
+    });
+};
+
+const dbDisconnect = () => {
+  return mongoose.disconnect();
+};
+
+const dbGet = () => {
+  return mongoose;
+};
+
+module.exports = {
+  dbConnect, 
+  dbDisconnect, 
+  dbGet
+};
--- server.js
+++ server.js
@@ -1,3 +1,72 @@
 'use strict';
 
 const express = require('express');
+const morgan = require('morgan');
+
+const { error404, error500 } = require('./error-middleware');
+const { PORT, CLIENT_ORIGIN } = require('./config');
+const { dbConnect } = require('./db-mongoose');
+
+// Create an Express application
+const app = express();
+
+// logs all requests
+app.use(
+  morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
+    skip: (req, res) => process.env.NODE_ENV === 'test'
+  })
+);
+
+// Create a static webserver
+app.use(express.static('public'));
+
+// Parse request body
+app.use(express.json());
+
+const cheeses = [
+  'Bath Blue',
+  'Barkham Blue',
+  'Buxton Blue',
+  'Cheshire Blue',
+  'Devon Blue',
+  'Dorset Blue Vinney',
+  'Dovedale',
+  'Exmoor Blue',
+  'Harbourne Blue',
+  'Lanark Blue',
+  'Lymeswold',
+  'Oxford Blue',
+  'Shropshire Blue',
+  'Stichelton',
+  'Stilton',
+  'Blue Wensleydale',
+  'Yorkshire Blue'
+];
+
+// Mount routers
+app.get('/api/cheeses', (req, res, next) => {
+
+  return res.json(cheeses);
+});
+
+// Error handlers
+app.use(error404);
+app.use(error500);
+
+const runServer = (port = PORT) => {
+  const server = app
+    .listen(port, () => {
+      console.info(`App listening on port ${server.address().port}`);
+    })
+    .on('error', err => {
+      console.error('Express failed to start');
+      console.error(err);
+    });
+};
+
+if (require.main === module) {
+  dbConnect();
+  runServer();
+}
+
+module.exports = { app };
